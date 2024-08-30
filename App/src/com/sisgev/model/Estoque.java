85c71dc1d2f287d09a19d53d3d412577e2266cae
igormanoels <s.igormanoel@gmail.com>
2024-08-29 23:51:40 -0300

    início do desenvolvimento das classes Estoque e Produto, e adequação do diagrama de classes (versão 1.0.1).

--- App/src/com/sisgev/model/Estoque.java
+++ App/src/com/sisgev/model/Estoque.java
@@ -1,5 +1,53 @@
 package com.sisgev.model;
 
-public class Estoque {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Estoque 
+{
+	private List<Produto> produtosList;
+	private int quantidade;
+	
+	public Estoque() {
+		this.produtosList = new ArrayList<>();
+	}
+	
+	public void cadastrarProduto(String...strProduto) throws IllegalArgumentException {
+		boolean verificar = verificaExistencia(Integer.parseInt(strProduto[0]));
+		if(verificar == false) {
+			produtosList.add(new Produto(strProduto[0], strProduto[1], strProduto[2], strProduto[3], strProduto[4]));
+			this.quantidade = Integer.parseInt(strProduto[5]);
+		} else {
+			throw new IllegalArgumentException("O produto já está cadastrado no sistema.");
+		}
+	}
+	
+	public void adcionarProduto() {
+		
+	}
+	
+	public void removerProduto() {
+		
+	}
+	
+	public void relatorioEstoque() {
+		
+	}
+	
+	private boolean verificaExistencia(int cod) {
+		for (Produto p : produtosList) {
+			if (p.getCodigoBarras() == cod) return true;
+		}
+		return false;
+	}
+	
+	
+	public int getQuantidade() {
+		return quantidade;
+	}
+	
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	
 }
--- App/src/com/sisgev/model/Produto.java
+++ App/src/com/sisgev/model/Produto.java
@@ -1,5 +1,70 @@
 package com.sisgev.model;
 
-public class Produto {
+public class Produto 
+{
+	private int codigoBarras;
+	private String nome;
+	private String marca;
+	private float preco;
+	private String validade;
+	
+	// Construtores da classe
+	public Produto() {}
+	
+	public Produto(String...strProduto) {
+		this.codigoBarras = Integer.parseInt(strProduto[0]);
+		this.nome = strProduto[1];
+		this.marca = strProduto[2];
+		this.preco= Float.parseFloat(strProduto[3]);
+		this.validade = strProduto[4];
+	}
 
+	
+	// Getters and Setters
+	public int getCodigoBarras() {
+		return codigoBarras;
+	}
+
+	public void setCodigoBarras(int codigoBarras) {
+		this.codigoBarras = codigoBarras;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public float getPreco() {
+		return preco;
+	}
+
+	public void setPreco(float precoUnitario) {
+		this.preco= precoUnitario;
+	}
+
+	public String getValidade() {
+		return validade;
+	}
+
+	public void setValidade(String validade) {
+		this.validade = validade;
+	}
+	
+	// To String
+	@Override
+	public String toString() {
+		return "Produto: " + nome + ", Marca: " + marca + ", Preço: " + preco;
+	}
+	
 }
