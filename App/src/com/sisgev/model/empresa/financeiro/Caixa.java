a6b9f69fe228bdef81fa810ac16dd092fcbd9ec2
igormanoels <s.igormanoel@gmail.com>
2024-11-04 23:44:56 -0300

    Continuação do desenvolvimento da classe caixa e vendas dos pacotes, com modificação no diagrama.

--- App/src/com/sisgev/model/empresa/financeiro/Caixa.java
+++ App/src/com/sisgev/model/empresa/financeiro/Caixa.java
@@ -1,5 +1,24 @@
 package com.sisgev.model.empresa.financeiro;
 
 public class Caixa {
+	
+	private double saldoCaixa = 0.0d;
+	private int numeroVendas = 0;
+	
+	public void sacarDoCaixa(String valor) {
+		this.saldoCaixa = this.saldoCaixa - (Integer.parseInt(valor));
+	}
+	
+	public void depositarNoCaixa(String valor) {
+		this.saldoCaixa = this.saldoCaixa + (Integer.parseInt(valor));
+	} 
+	
+	public String relatorioCaixa() {
+		return "Saldo em caixa: R$ " + this.saldoCaixa + "\nNúmero de Vendas: " + this.numeroVendas;
+	}
+	
+	public void registrarVenda() {
+		this.numeroVendas += 1;
+	}
 
 }
--- App/src/com/sisgev/model/empresa/financeiro/Vendas.java
+++ App/src/com/sisgev/model/empresa/financeiro/Vendas.java
@@ -1,5 +1,31 @@
 package com.sisgev.model.empresa.financeiro;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.sisgev.model.empresa.logistica.Produto;
+
 public class Vendas {
+	
+	private Caixa caixa = new Caixa();
+	private List<Produto> produtos;
+	
+	public Vendas() {
+		this.produtos = new ArrayList<>();
+	}
+	
+	public void adcionarProduto() {
+		
+	}
+	
+	public void removerProduto() {}
+	
+	public void cancelarCompra() {}
+	
+	public void finalizarCompra() {
+		this.caixa.registrarVenda();
+	}
+	
+	public void imprimirRecibo() {}
 
 }
